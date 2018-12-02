4b2d9b6dad7ef9334ea3b9b29143c64aed11eaee
Haodi Sun <sunhdshd@gmail.com>
2018-12-02 01:27:29 -0500

    Revert "Merge branch 'master' of https://github.com/KongfuMan/ECommerce"
    This reverts commit a97e5a67319dba4b61687831880e40fec238493b, reversing
    changes made to d0c084e11076e9bab7b59a77be6c9a195d6335ef.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,6 @@
 import {combineReducers} from 'redux'
 import authReducer from "./authReducer";
-import productReducer from './productReducer'
 
 export default combineReducers({
-    auth : authReducer,
-    products: productReducer
+    auth : authReducer
 });
